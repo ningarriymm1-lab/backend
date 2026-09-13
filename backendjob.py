06834e78089b0e31518d5a0fb95a796da6863b42
thanakron <ningarriymm1@gmail.com>
2026-09-13 20:27:50 +0700

    Update backendjob.py

--- backendjob.py
+++ backendjob.py
@@ -346,4 +346,8 @@ def update_title():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+else:
+    # รันผ่าน Gunicorn บน Render
+    init_db()
